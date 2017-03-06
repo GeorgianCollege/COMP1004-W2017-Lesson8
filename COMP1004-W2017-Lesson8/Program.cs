55ce9571c8f2c917ef460acb5ab21afbcc968aa0
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-03-06 15:32:32 -0500

    created data project

--- COMP1004-W2017-Lesson8/Program.cs
+++ COMP1004-W2017-Lesson8/Program.cs
@@ -16,7 +16,7 @@ namespace COMP1004_W2017_Lesson8
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DataForm());
         }
     }
 }
